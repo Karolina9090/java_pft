ee98029153d62c352e8b840519e12e4bb46bd7e0
Karolina9090 <44869292+Karolina9090@users.noreply.github.com>
2018-12-20 19:05:35 +0100

    Paratremization of test methods

--- adressbook-web-tests/src/test/java/pl/stqua/pft/adressbook/tests/ContactCreationTests.java
+++ adressbook-web-tests/src/test/java/pl/stqua/pft/adressbook/tests/ContactCreationTests.java
@@ -1,10 +1,15 @@
 package pl.stqua.pft.adressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pl.stqua.pft.adressbook.model.ContactData;
 import pl.stqua.pft.adressbook.model.Contacts;
+import pl.stqua.pft.adressbook.model.GroupData;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -24,12 +29,21 @@ public class ContactCreationTests extends TestBase {
             before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
 
-  @Test(enabled = false)
-  public void testBadContactCreation() {
+
+  @DataProvider
+  public Iterator<Object[]> validContacts() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[]{new ContactData().withFirstname("test1").withLastname("test2").withAdress("0000000001").withHome("test3").withEmail("test@test1")});
+    list.add(new Object[]{new ContactData().withFirstname("test2").withLastname("test3").withAdress("0000000002").withHome("test4").withEmail("test@test2")});
+    list.add(new Object[]{new ContactData().withFirstname("test3").withLastname("test4").withAdress("0000000003").withHome("test5").withEmail("test@test3")});
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validContacts")
+  public void testBadContactCreation(ContactData contact) {
+    app.contact().create(contact, false);
     app.goTo().goToAddNewContact();
     Contacts before = app.contact().all();
-    ContactData contact = new ContactData().withFirstname("test1'").withLastname("test2'").withAdress("000000000").withHome("test3").withEmail("test@test");
-    app.contact().create(contact, false);
     assertThat(app.contact().count(), equalTo(before.size()));
     Contacts after = app.contact().all();
     assertThat(after, equalTo(before));
--- adressbook-web-tests/src/test/java/pl/stqua/pft/adressbook/tests/GroupCreationTest.java
+++ adressbook-web-tests/src/test/java/pl/stqua/pft/adressbook/tests/GroupCreationTest.java
@@ -5,6 +5,9 @@ import pl.stqua.pft.adressbook.model.GroupData;
 import pl.stqua.pft.adressbook.model.Groups;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -24,17 +27,25 @@ public class GroupCreationTest extends TestBase {
 
   }
 
-  @Test(enabled = false)
-  public void testBadGroupCreation() {
+  @DataProvider
+  public Iterator<Object[]> validGroups() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[]{new GroupData().withName("test1").withHeader("header1").withFooter("footer1")});
+    list.add(new Object[]{new GroupData().withName("test2").withHeader("header2").withFooter("footer2")});
+    list.add(new Object[]{new GroupData().withName("test3").withHeader("header2").withFooter("footer3")});
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validGroups")
+  public void testBadGroupCreation(GroupData group) {
     app.goTo().groupPage();
     Groups before = app.group().all();
-    GroupData group = new GroupData().withName("test2'");
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size()));
     Groups after = app.group().all();
-    assertThat(after,equalTo(before));
-
+    assertThat(after, equalTo(before));
   }
+
 
   @Test
   public void testGroupCreation2() {
@@ -45,5 +56,6 @@ public class GroupCreationTest extends TestBase {
     app.group().submitGroupCreation();
     app.group().returnToGroupPage();
   }
+
 
 }

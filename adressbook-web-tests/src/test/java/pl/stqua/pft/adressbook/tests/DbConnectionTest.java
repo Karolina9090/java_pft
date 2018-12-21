411f682c8520482258ec03379b6d129c602e67e5
Karolina9090 <44869292+Karolina9090@users.noreply.github.com>
2018-12-21 21:47:07 +0100

    Database driver. Receiving data from DB for contacts.

--- adressbook-web-tests/src/test/java/pl/stqua/pft/adressbook/tests/DbConnectionTest.java
+++ adressbook-web-tests/src/test/java/pl/stqua/pft/adressbook/tests/DbConnectionTest.java
@@ -1,6 +1,8 @@
 package pl.stqua.pft.adressbook.tests;
 
 import org.testng.annotations.Test;
+import pl.stqua.pft.adressbook.model.ContactData;
+import pl.stqua.pft.adressbook.model.Contacts;
 import pl.stqua.pft.adressbook.model.GroupData;
 import pl.stqua.pft.adressbook.model.Groups;
 
@@ -25,6 +27,33 @@ public class DbConnectionTest {
       st.close();
       conn.close();
       System.out.println(groups);
+
+    } catch (SQLException ex) {
+      // handle any errors
+      System.out.println("SQLException: " + ex.getMessage());
+      System.out.println("SQLState: " + ex.getSQLState());
+      System.out.println("VendorError: " + ex.getErrorCode());
+    }
+  }
+
+  @Test
+  public void testDbConnectionContact() {
+    Connection conn = null;
+    try {
+      conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=");
+      Statement st = conn.createStatement();
+      ResultSet rs = st.executeQuery("select id,firstname,lastname,address,email from addressbook");
+      Contacts contacts = new Contacts();
+      while (rs.next()) {
+        contacts.add(new ContactData().withId(rs.getInt("id")).withFirstname(rs.getString("firstname"))
+                .withLastname(rs.getString("lastname")).withAdress(rs.getString("address"))
+                .withEmail(rs.getString("email")));
+
+      }
+      rs.close();
+      st.close();
+      conn.close();
+      System.out.println(contacts);
 
     } catch (SQLException ex) {
       // handle any errors

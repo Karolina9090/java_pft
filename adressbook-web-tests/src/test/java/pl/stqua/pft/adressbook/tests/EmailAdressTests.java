4ad9489d5ccea07d4c6cb468a267729995d395ef
Karolina9090 <44869292+Karolina9090@users.noreply.github.com>
2019-01-15 22:14:53 +0100

    Solution to task number 13: perform a test for checking the page with detailed contact information

--- adressbook-web-tests/src/test/java/pl/stqua/pft/adressbook/tests/EmailAdressTests.java
+++ adressbook-web-tests/src/test/java/pl/stqua/pft/adressbook/tests/EmailAdressTests.java
@@ -10,6 +10,34 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class EmailAdressTests extends TestBase {
-  
+
+  @Test(enabled = false)
+  public void EmailAdress() {
+    app.goTo().homePage();
+    ContactData contact = app.contact().all().iterator().next();
+    ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+
+    assertThat(contact.getAllEmailAdress(), equalTo(mergeEmailAdress(contactInfoFromEditForm)));
+  }
+
+  private String mergeEmailAdress(ContactData contact) {
+    return Arrays.asList(contact.getEmailAdress(), contact.getEmailAdress2(), contact.getEmailAdress3())
+            .stream().filter((s) -> ! s.equals(""))
+            .map(EmailAdressTests::cleaned)
+            .collect(Collectors.joining("\n"));
+  }
+
+  public static String cleaned(String emailAdress) {
+    return emailAdress.replaceAll("\\s", "").replaceAll("[-()]", "");
+  }
+
+  @Test
+  public void EmailAdressOnDetailsContactPage() {
+    app.goTo().homePage();
+    ContactData contact = app.contact().all().iterator().next();
+    ContactData contactInfoFromDetailsForm = app.contact().infoFromDetailsForm(contact);
+
+    assertThat(contact.getAllEmailAdress(), equalTo(mergeEmailAdress(contactInfoFromDetailsForm)));
+  }
 
 }
